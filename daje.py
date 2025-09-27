32a52fbe28af63972b15350344c618afd3990d74
FireBlaze360308 <x.block360308@gmail.com>
2025-09-27 19:46:41 +0200

    Update daje.py

--- daje.py
+++ daje.py
@@ -1,24 +1,22 @@
 import mpmath
 import sys
 sys.set_int_max_str_digits(int(10E5))
+mpmath.mp.dps = 1_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000
 
 def main(number_start) -> None:
     current_number = number_start
-    mydict = []
     power_programme: int = 1
     counter: int = 0
-    print(current_number)
     while True:
         if current_number % 2 == 0:
-            current_number: int = int(mpmath.ceil(mpmath.mp.mpf(current_number) / mpmath.mp.power(2, power_programme)))
+            current_number: int = int(mpmath.floor(mpmath.mp.mpf(current_number) / mpmath.mp.power(2, power_programme)))
             counter += 1
         else:
             current_number: int = int(mpmath.mp.power(3, power_programme) * current_number + 1)
             counter += 1
-        print(current_number)
         if current_number == 1 or current_number == 0:
             break
-    print(f"daje -> {power_programme, counter+1}")
+    print(f"result -> {power_programme, counter+1}")
 
 if __name__ == '__main__':
-    main(10E140)
+    main(1)
