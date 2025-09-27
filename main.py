da57f3ff43b93fec763b73736859ebd776eff004
FireBlaze360308 <x.block360308@gmail.com>
2025-09-27 19:11:05 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+#Imports
+import csv
+import mpmath
+
+#Float precision
+mpmath.mp.dps = 1_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000
+
+#Collatz Function
+def function1(number_start: int) -> None:
+
+    power_programme: int = 1
+
+    while True:
+        counter: int = 0
+        current_number: int = number_start
+        while True:
+            if current_number % 2 == 0:
+                current_number: int = int(mpmath.floor(mpmath.mp.mpf(current_number) / mpmath.mp.power(2, power_programme)))
+
+            else:
+                current_number: int = mpmath.mp.power(3, power_programme) * current_number + 1
+
+            counter += 1
+
+            if current_number == 1 or current_number == 0:
+                break
+
+        with open("result_int_division.csv", "a", newline="") as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=["power", "steps"])
+            writer.writerows([{"power": power_programme, "steps": counter+1}])
+
+        power_programme += 1
+
+#Main Function
+def main() -> None:
+    function1(1)
+
+#Start
+if __name__ == '__main__':
+    main()
