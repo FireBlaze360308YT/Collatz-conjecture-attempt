e6b07a5d0ac7ddef9c616172992701aad3840eae
FireBlaze360308 <x.block360308@gmail.com>
2025-09-27 19:48:48 +0200

    Update daje.py

--- daje.py
+++ daje.py
@@ -4,17 +4,16 @@ sys.set_int_max_str_digits(int(10E5))
 mpmath.mp.dps = 1_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000
 
 def main(number_start) -> None:
-    current_number = number_start
     power_programme: int = 1
     counter: int = 0
     while True:
-        if current_number % 2 == 0:
-            current_number: int = int(mpmath.floor(mpmath.mp.mpf(current_number) / mpmath.mp.power(2, power_programme)))
+        if number_start % 2 == 0:
+            number_start: int = int(mpmath.floor(mpmath.mp.mpf(number_start) / mpmath.mp.power(2, power_programme)))
             counter += 1
         else:
-            current_number: int = int(mpmath.mp.power(3, power_programme) * current_number + 1)
+            number_start: int = int(mpmath.mp.power(3, power_programme) * number_start + 1)
             counter += 1
-        if current_number == 1 or current_number == 0:
+        if number_start == 1 or number_start == 0:
             break
     print(f"result -> {power_programme, counter+1}")
 
