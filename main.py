63b19cbea939fbf6151f1c568de3492bca056d7e
FireBlaze360308 <x.block360308@gmail.com>
2025-09-28 17:45:23 +0200

    Update main.py
    Improved performance by greatly improving csv file handling

--- main.py
+++ main.py
@@ -6,34 +6,32 @@ import mpmath
 mpmath.mp.dps = 1_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000
 
 #Collatz Function
-def function1(number_start: int) -> None:
+def function1(number_start: int = 1, power_programme: int = 1) -> None:
 
-    power_programme: int = 1
-
-    while True:
-        counter: int = 0
-        current_number: int = number_start
+    with open("result_int_division.csv", "a", newline="") as csvfile:
         while True:
-            if current_number % 2 == 0:
-                current_number: int = int(mpmath.floor(mpmath.mp.mpf(current_number) / mpmath.mp.power(2, power_programme)))
+            counter: int = 0
+            current_number: int = number_start
+            while True:
+                if current_number % 2 == 0:
+                    current_number: int = int(mpmath.floor(mpmath.mp.mpf(current_number) / mpmath.mp.power(2, power_programme)))
 
-            else:
-                current_number: int = mpmath.mp.power(3, power_programme) * current_number + 1
+                else:
+                    current_number: int = mpmath.mp.power(3, power_programme) * current_number + 1
 
-            counter += 1
+                counter += 1
 
-            if current_number == 1 or current_number == 0:
-                break
+                if current_number == 1 or current_number == 0:
+                    break
 
-        with open("result_int_division.csv", "a", newline="") as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=["power", "steps"])
             writer.writerows([{"power": power_programme, "steps": counter+1}])
 
-        power_programme += 1
+            power_programme += 1
 
 #Main Function
 def main() -> None:
-    function1(1)
+    function1(1, 1)
 
 #Start
 if __name__ == '__main__':
